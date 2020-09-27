471ae55610d3aa2cee374bdefd836cd963a62699
georgeliASC20 <68346402+georgeliASC20@users.noreply.github.com>
2020-09-26 21:11:21 -0400

    Update script.js

--- SortedArray/script.js
+++ SortedArray/script.js
@@ -1,27 +1,27 @@
-const { array } = require("prop-types");
-
-
 function myfunction(numbersarray, target){
-    var x = -1;
-    var y = -1
+    var x = -1; //Initialize functions
+    var y = -1;
+    //Used to a for loop to traverse through the arrays
     for(var i = 0; i<numbersarray.length; i++){
 
         
         if(target == numbersarray[i]){
-            if(x == -1){
-                x = numbersarray[i]
+            if(x == -1){//The position of the first target point will be X
+                x = i
             }
             else{
-                y = numbersarray[i]
+                y = i//The position of the second target point will be y
             }
         }
     }
-    console.log(numbersarray, x, y)
+    console.log("Input: nums = ",numbersarray, "target = ", target)
+    console.log("Output: [", x, ",",y, "]")
 
 }
+//I call the functions and Initialize them
 nums = [5,7,7,8,8,10]
 let x = myfunction(nums, 8)
-
+let y = myfunction(nums, 6)
 
 
 
